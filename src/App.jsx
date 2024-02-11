34333144872dddfaca3cb18816d777ae7f876494
Julian Mai <julian@julians-mbp>
2024-02-10 21:26:31 -0800

    implemented toggle checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -27,9 +27,39 @@ export default function App() {
       ]  
     })
 
-    // Will reset the input
+
+    // Reset the input on submit
     setNewItem("")
   }
+
+  // want to update todos 'completed' property
+  function toggleTodo (id, completed, ) {
+    // call the setter function to updaate state
+    // because we care about the current value, we pass in a function like this
+    // This approach is recommended when the new state depends on the old state, ensuring you're working with the most recent state.
+    setTodos(currentTodos => {
+      // .map() evaluates to an a new array created by mapping over 'currentTodos'
+      return currentTodos.map(todo => {
+        // for each todo item
+        // checking if its 'id' matches the 'id' parameter passed to 'toggleTodo'
+        if (todo.id === id) {
+          // if the 'if' of the current 'todo' matches the 'id' passed to the function, a new object is created and returned
+          // spreads the properties of the current todo item
+          // updates the 'completed' property to the 'toggleTodo' function
+          return { ...todo, completed }
+        }
+
+        // if the 'id' of the current todo item does not match the 'id' passed into the function, the 'todo' item is returned unchanged
+        // returning unchanged todo items
+        return todo;
+      })
+    })
+  }
+  // The purpose of the toggleTodo function is to toggle the completion status of a specific todo item in the todos array. 
+  // By using the map method, it ensures that a new array is returned (as required by React)...
+  // with all items remaining the same except for the one that matches the specified id, which gets its completed status updated.
+  // This pattern ensures immutability of the state, a key principle in React development, by creating a new array instead of modifying the existing one directly, thus enabling React to efficiently detect changes and update the UI accordingly.
+
 
   return (
     <>
@@ -59,7 +89,8 @@ export default function App() {
           // each iterated tag needs key
           return <li key={todo.id}>
           <label>
-            <input type="checkbox" checked={todo.completed}/>
+            {/* when checkbox is changed, invoke function toggleTodo. the function takes two arguments. the todo.id and the todo.completed */}
+            <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
             {todo.title}
           </label>
           <button className="btn btn-danger">Delete</button>
@@ -69,5 +100,3 @@ export default function App() {
     </>
   )
 }
-
-// 12m30s
