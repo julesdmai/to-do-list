bba55e8925e95532677507df7d82837e347e7ddf
Julian Mai <julian@julians-mbp>
2024-02-10 22:22:37 -0800

    completed TodoItem component

---
+++ src/TodoItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+// when checkbox is changed, invoke function toggleTodo. the function takes two arguments. the todo.id and the todo.completed
+// onClick needs to invoke an anonymous function returning the function invocation of deleteTodo passing in todo.id. onClick needs an arrow function. you don't want to pass in the result of calling deleteTodo
+export default function TodoItem({ completed, id, title }) {
+    return (
+        <li>
+            <label>
+                <input
+                    type="checkbox"
+                    checked={completed}
+                    // onChange={e => toggleTodo(id, e.target.checked)}
+                />
+                {title}
+            </label>
+            {/* <button onClick={() => deleteTodo(id)} className="btn btn-danger"
+            >
+                Delete
+            </button> */}
+        </li>
+    )
+}
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import TodoItem from "./TodoItem";
 
 export default function TodoList({ todos }) {
     return (
@@ -9,22 +10,10 @@ export default function TodoList({ todos }) {
             In React, arrays are rendered as elements one after another */}
             {todos.map(todo => {
                 // each iterated tag needs key
-                return <li key={todo.id}>
-                    <label>
-                        {/* when checkbox is changed, invoke function toggleTodo. the function takes two arguments. the todo.id and the todo.completed */}
-                        <input
-                            type="checkbox"
-                            checked={todo.completed}
-                            // onChange={e => toggleTodo(todo.id, e.target.checked)}
-                        />
-                        {todo.title}
-                    </label>
-                    {/* onClick needs to invoke an anonymous function returning the function invocation of deleteTodo passing in todo.id
-                    onClick needs an arrow function
-                    you don't want to pass in the result of calling deleteTodo
-                    */}
-                    {/* <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button> */}
-                </li>
+                // spread props of todo
+                return (
+                    <TodoItem {...todo} key={todo.id} />
+                );
             })}
         </ul>
     )
